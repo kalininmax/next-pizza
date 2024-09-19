16cd550c32ef8541961cd2fc3d1e525e2c587e5b
Maksim Kalinin <kalinin.m.a@bk.ru>
2024-09-19 20:20:26 +0300

    Try to fix deploy #2

--- services/api.ts
+++ services/api.ts
@@ -2,9 +2,9 @@ import * as products from './products';
 import * as ingredients from './ingredients';
 
 export enum ApiRoutes {
-	PRODUCTS = 'products',
-	PRODUCTS_SEARCH = 'products/search',
-	INGREDIENTS = 'ingredients',
+	PRODUCTS = '/products',
+	PRODUCTS_SEARCH = '/products/search',
+	INGREDIENTS = '/ingredients',
 }
 
 export const Api = {
